6dfb52fd48b5a99d8d20b9811f18421db7e25219
nikkikim <67214934+nikkikim@users.noreply.github.com>
2021-04-17 21:33:37 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,99 @@
+function toggleMenu() {
+
+    let x = document.getElementById("links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+    let m = document.getElementById("bars");
+    let ic = document.getElementById("icon");
+    let sic = document.getElementById("soundIcon");
+    if (m.src.endsWith("images/menu.png")) {
+        m.src = "images/cross.png";
+        ic.style.backgroundColor = "rgba(34, 34, 34, 0.5)";
+        sic.style.backgroundColor = "rgba(34, 34, 34, 0.5)";
+    } else {
+        m.src = "images/menu.png";
+        ic.style.backgroundColor = "black";
+        sic.style.backgroundColor = "black";
+    }
+}
+function toggleSound() {
+
+    let m = document.getElementById("soundBars");
+    let s = document.getElementById("bgm");
+    if (m.src.endsWith("images/soundOff.png")) {
+        m.src = "images/soundOn.png";
+        s.play();
+    } else {
+        m.src = "images/soundOff.png";
+        s.pause();
+    }
+}
+var isMobile = true;
+// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+//     // true for mobile device
+//     isMobile = true;
+//     document.write("mobile device");
+//     console.log("mobile");
+// }else{
+//     // false for not mobile device
+//     isMobile = false;
+//     document.writeln("<div style='position:fixed;width:100%;height:100%;top:150px;left:0px;background-color:black;'><img style='width:50%;margin:0 auto;display:block;' src='ma.jpg'></div>");
+//     // window.stop();
+// }
+
+$(window).scroll(function() {
+    $('video').each(function(){
+        if ($(this).visible(true)) {
+            $(this)[0].play();
+        } else {
+            $(this)[0].pause();
+        }
+    });
+    // $('.chapter').each(function(){
+    //     if ($(this).visible(true)){
+    //         // this.id;
+    //     }
+    // });
+});
+
+window.onload = function (){
+    if (isMobile){
+        console.log("ismobile");
+        let container = document.getElementById('content2');
+        let slider = document.getElementById('slider');
+        document.addEventListener('click', function( event ) {
+            if (container !== event.target && !container.contains(event.target)) {
+                console.log('clicking outside the div');
+            } else {
+                let sList = slider.classList;
+                let isOpen = slider.classList.contains('slide-in');
+                slider.classList.remove("slide-in");
+                slider.classList.remove("slide-out");
+
+                slider.classList.add(isOpen ? 'slide-out' : 'slide-in');
+                console.log("inside div@@@");
+            }
+        });
+
+        if (document.getElementById('video2') != undefined){
+            document.getElementById('video2').addEventListener('ended', videoHandler, false);
+
+            // document.getElementById('video2').style.display = 'none';
+            // document.getElementById('content2').style.display = 'block';
+        } else {
+            document.getElementById('content2').style.display = 'block';
+            console.log("No video");
+        }
+    } else {
+        document.getElementById('contents').style.display = 'none';
+    }
+};
+function videoHandler(e) {
+    // What you want to do after the event
+    document.getElementById('content2').style.display = 'block';
+    document.getElementById('video2').style.display = 'none';
+}
