63acc2881206ce80f4257df8573cafb06c0067d0
nikkikim <67214934+nikkikim@users.noreply.github.com>
2021-05-12 02:42:24 -0700

    Fix before load click

--- script.js
+++ script.js
@@ -51,17 +51,17 @@ function toggleSound() {
 
 var hasStarted = false;
 var isMobile = true;
-// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-//     // true for mobile device
-//     isMobile = true;
-//     document.write("mobile device");
-//     console.log("mobile");
-// }else{
-//     // false for not mobile device
-//     isMobile = false;
-//     document.writeln("<div style='position:fixed;width:100%;height:100%;top:150px;left:0px;background-color:black;'><img style='width:50%;margin:0 auto;display:block;' src='ma.jpg'></div>");
-//     // window.stop();
-// }
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // true for mobile device
+    isMobile = true;
+    document.write("mobile device");
+    console.log("mobile");
+}else{
+    // false for not mobile device
+    isMobile = false;
+    document.writeln("<div style='position:fixed;width:100%;height:100%;top:150px;left:0px;background-color:black;'><img style='width:50%;margin:0 auto;display:block;' src='ma.jpg'></div>");
+    // window.stop();
+}
 
 window.onload = function (){
     if (document.getElementById('video2') != undefined){
@@ -122,6 +122,16 @@ var i;
 document.addEventListener('click', function( event ) {
     if (isClickBackground(event)) {
         console.log("isssss");
+    } else {
+        let slider = document.getElementById('slider');
+        if (slider != undefined){
+            let sList = slider.classList;
+            let isOpen = slider.classList.contains('slide-in');
+            slider.classList.remove("slide-in");
+            slider.classList.remove("slide-out");
+
+            slider.classList.add(isOpen ? 'slide-out' : 'slide-in');
+        }
     }
 });
 
@@ -130,18 +140,6 @@ window.onload = function (){
         console.log("ismobile");
         let container = document.getElementById('content2');
         let slider = document.getElementById('slider');
-        document.addEventListener('click', function( event ) {
-            if (isClickBackground(event)) {
-                console.log("isssss");
-            } else {
-                let sList = slider.classList;
-                let isOpen = slider.classList.contains('slide-in');
-                slider.classList.remove("slide-in");
-                slider.classList.remove("slide-out");
-
-                slider.classList.add(isOpen ? 'slide-out' : 'slide-in');
-            }
-        });
 
         if (document.getElementById('video2') != undefined){
             document.getElementById('video2').addEventListener('ended', videoHandler, false);
